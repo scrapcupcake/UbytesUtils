67c3139848808e016e369d6fff7cb40a90f93a4f
Hunter Thomas <ScrapCupcake@gmail.com>
2015-02-14 17:35:02 -0800

    Added an example EditorWindow

---
+++ Assets/UBytesUtil/Editor/Examples/AssetCreateWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UBytes.Utils;
+
+public class AssetCreateWindow : EditorWindow
+{
+	public static AssetCreateWindow instance;
+
+	ScriptableObject target;
+
+	[MenuItem("Examples/Create Generic Asset #G")]
+	public static void OpenWindow ()
+	{
+		instance = EditorWindow.GetWindow<AssetCreateWindow> ("Asset Creation Editor");
+	}
+
+	public void OnGUI ()
+	{
+		if (GUILayout.Button ("New Generic Asset")) {
+			var newObj = new ScriptableObject ();
+			AssetBank.instance.SaveAsset (newObj, "Example.asset");
+			target = newObj;
+		}
+
+		target = EditorGUILayout.ObjectField (target, typeof(ScriptableObject), false) as ScriptableObject;
+	}
+}
